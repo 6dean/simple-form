d277303ba4c4b2e0a7d17773d0bb3cdcfb06c733
6dean <ferdjallahrockdean@gmail.com>
2022-11-03 17:42:19 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -9,74 +9,74 @@ function App() {
   const [infos, setInfos] = useState(false);
 
   return (
-    <div className="App">
-      <header>
-        <p>create Account </p>
-      </header>
-      <div className={infos === true && "undisplay"}>
-        <div>
-          <input
-            onChange={(name) => setName(name.target.value)}
-            type="text"
-            placeholder="Jean Dupont"
-            name="name"
-            value={name}
-          />
+    <div className="formulaire">
+      <div className="App">
+        <header>
+          <p>create Account </p>
+        </header>
+        <div className={infos === true && "undisplay"}>
+          <div>
+            <input
+              onChange={(name) => setName(name.target.value)}
+              type="text"
+              placeholder="Jean Dupont"
+              name="name"
+              value={name}
+            />
+          </div>
+          <div>
+            <input
+              onChange={(elem) => setEmail(elem.target.value)}
+              type="email"
+              placeholder="jeandupont@lereacteur.io"
+              name="email"
+              value={email}
+            />
+          </div>
+          <div>
+            <input
+              onChange={(elem) => setPassword(elem.target.value)}
+              type="password"
+              placeholder="lErAcTeUr2022"
+              name="password"
+              value={password}
+            />
+          </div>
+          <div>
+            <input
+              onChange={(elem) => setConfirmPass(elem.target.value)}
+              type="password"
+              placeholder="lErAcTeUr2022"
+              name="confirm password"
+              value={confirmPass}
+            />
+          </div>
+          <div>
+            <button
+              onClick={() => {
+                if (password !== confirmPass) {
+                  alert(`Password is not the same`);
+                } else {
+                  setInfos(true);
+                }
+              }}
+            >
+              envoyez
+            </button>
+          </div>
         </div>
-        <div>
-          <input
-            onChange={(elem) => setEmail(elem.target.value)}
-            type="email"
-            placeholder="jeandupont@lereacteur.io"
-            name="email"
-            value={email}
-          />
-        </div>
-        <div>
-          <input
-            onChange={(elem) => setPassword(elem.target.value)}
-            type="password"
-            placeholder="lErAcTeUr2022"
-            name="password"
-            value={password}
-          />
-        </div>
-        <div>
-          <input
-            onChange={(elem) => setConfirmPass(elem.target.value)}
-            type="password"
-            placeholder="lErAcTeUr2022"
-            name="confirm password"
-            value={confirmPass}
-          />
-        </div>
-        <div>
-          <input
-            type="submit"
-            onSubmit={(event) => {
-              event.preventdefault();
-              if (password !== confirmPass) {
-                <alert>"Votre mot de passe n'est pas identique"</alert>;
-              } else {
-              }
-            }}
+        <div className={infos === true ? "display" : "undisplay"}>
+          <div>name:{name}</div>
+          <div>email:{email}</div>
+          <div>password:{password}</div>
+          <button
             onClick={() => {
-              setInfos(true);
+              setInfos(false);
             }}
-          />
+          >
+            EDITER
+          </button>
         </div>
-      </div>
-      <div className={infos === true ? "display" : "undisplay"}>
-        <div>name:{name}</div>
-        <div>email:{email}</div>
-        <div>password:{password}</div>
-        <button
-          onClick={() => {
-            setInfos(false);
-          }}
-        >
-          EDITER
-        </button>
       </div>
     </div>
   );
